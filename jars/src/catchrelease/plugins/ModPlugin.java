e380abfcfde7e1b79735c1ba2d9ec55d2ed8875a
SirHartley <sirhartley.default@gmail.com>
2025-12-14 14:57:00 +0100

    Initial Commit

---
+++ jars/src/catchrelease/plugins/ModPlugin.java
@@ -0,0 +1,11 @@
+package catchrelease.plugins;
+
+import com.fs.starfarer.api.BaseModPlugin;
+
+public class ModPlugin extends BaseModPlugin {
+    @Override
+    public void onGameLoad(boolean newGame) {
+        super.onGameLoad(newGame);
+
+    }
+}
