bf7ea0b49322e835a0abce63e0ac0c425aad4b25
SirHartley <sirhartley.default@gmail.com>
2026-08-20 22:20:33 +0200

    Use a generic portrait for the child tournament

--- jars/src/catchrelease/campaign/fish/jobs/KidsJob.java
+++ jars/src/catchrelease/campaign/fish/jobs/KidsJob.java
@@ -23,6 +23,8 @@ import java.util.Map;
  */
 public class KidsJob extends FishJob {
 
+    public static final String GROUP_PORTRAIT = "graphics/portraits/portrait_generic.png";
+
     /** The flag that puts this job's own pair of options up instead of the shared hand-over. */
     public static final String CHOICE_FLAG = "$catchrelease_duelChoice";
 
@@ -49,6 +51,9 @@ public class KidsJob extends FishJob {
         setGiverVoice(Voices.SPACER);
 
         if (!setUpGiver(createdAt)) return false;
+        //The giver is a mission anchor, not one of the two children: the scene gets no lone face.
+        getPerson().setPortraitSprite(GROUP_PORTRAIT);
+
 
         days = DAYS;
 
